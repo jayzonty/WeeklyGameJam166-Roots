17f8e27712ba7ddf4df3c6881c33ad3ba68447aa
Jayzon Ty <jayzon.ty@gmail.com>
2020-09-14 16:20:28 +0800

    Added UI to display some game state variables

---
+++ Assets/Scripts/UI/InfoPanelBehavior.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace WGJRoots
+{
+    public class InfoPanelBehavior : MonoBehaviour
+    {
+        public Text growthLevelText;
+
+        public Text nutrientPointsText;
+
+        public Text branchPointsText;
+
+        public GameState gameState;
+
+        private void Update()
+        {
+            if (growthLevelText != null)
+            {
+                growthLevelText.text = "Growth Level: " + gameState.growthLevel;
+            }
+
+            if (nutrientPointsText != null)
+            {
+                nutrientPointsText.text = "Nutrient Points: " + gameState.nutrientPoints + "/" + gameState.nutrientPointsToNextGrowthLevel;
+            }
+
+            if (branchPointsText != null)
+            {
+                branchPointsText.text = "Branch Points: " + gameState.BranchPoints;
+            }
+        }
+    }
+}
