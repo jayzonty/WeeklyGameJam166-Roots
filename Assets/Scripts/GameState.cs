c9bd0358d2c5cbd175816363001ead0a776ad338
Jayzon Ty <jayzon.ty@gmail.com>
2020-09-14 10:30:06 +0800

    Added a class to hold the current game state

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WGJRoots
+{
+    public class GameState : MonoBehaviour
+    {
+        public int growthLevel;
+
+        public int nutrientPoints;
+        public int nutrientPointsToNextGrowthLevel;
+    }
+}
