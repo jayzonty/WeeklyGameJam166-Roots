064d5fd0465652ebcde9cd8633b4e3418dc45301
Jayzon Ty <jayzon.ty@gmail.com>
2020-09-16 12:24:05 +0800

    Added a title scene

---
+++ Assets/Scripts/UI/StartGameButtonBehavior.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace WGJRoots
+{
+    public class StartGameButtonBehavior : MonoBehaviour
+    {
+        public string targetScene = "";
+
+        private Button button;
+
+        private void Awake()
+        {
+            button = GetComponent<Button>();
+        }
+
+        private void OnEnable()
+        {
+            button.onClick.AddListener(GoToTargetScene);
+        }
+
+        private void OnDisable()
+        {
+            button.onClick.RemoveListener(GoToTargetScene);
+        }
+
+        private void GoToTargetScene()
+        {
+            SceneManager.LoadScene(targetScene);
+        }
+    }
+}
